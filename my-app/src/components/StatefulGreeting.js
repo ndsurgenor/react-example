3eda1a7240d0fdcc2778d66001145d3d595f4e56
Nathan Surgenor <ndsurgenor@gmail.com>
2023-11-27 22:27:29 +0000

    Add count button

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -7,21 +7,30 @@ class StatefulGreeting extends React.Component {
         this.state= {
             introduction: "Hello!",
             buttonText: "Exit",
+            buttonCount: 0,
         };
     }
 
     handleClick() {
         this.setState({
             introduction: this.state.introduction === "Hello!" ? "Goodbye!" : "Hello!",
-            buttonText: this.state.buttonText === "Enter" ? "Exit" : "Enter",
+            buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
         });
+    }
+
+    countClick() {
+        this.setState({
+            buttonCount: ++this.state.buttonCount,
+        })
     }
     
     render() {
         return (
             <div>
                 <h1>{this.state.introduction} {this.props.greeting}</h1>
-                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>                
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={() => this.countClick()}>Count</button>
+                <h2>The count button has been clicked {this.state.buttonCount} times</h2>                
             </div>
         )
     }
