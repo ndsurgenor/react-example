7146988c26a338a375aa7e0dc3670f2ff2e9d791
Nathan Surgenor <ndsurgenor@gmail.com>
2023-11-28 22:14:01 +0000

    Split component into UserData and UserMessage

--- my-app/src/components/NestingComponents.js
+++ my-app/src/components/NestingComponents.js
@@ -1,34 +1,26 @@
 import React, { Component } from 'react'
+import UserMessage from './UserMessage'
+import UserData from './UserData'
 
 export class NestingComponents extends Component {
 
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-      isLoaded: true,
-      isLoggedIn: true,
-    }
-  }
+    constructor(props) {
+        super(props)
 
-  render() {
-    return (
-      <div>
-        <h1>{this.state.isLoaded ? "Data Loaded" : "Loading..."}</h1>
-        {this.state.isLoggedIn ? (
-          <div>
-            <p>Welcome to the site! Please complete the steps below:</p>
-            <ol>
-              <li>Confirm your email</li>
-              <li>Complete your profile</li>
-              <li>Subscribe to our newsletter</li>
-            </ol>
-          </div>
-        ) :
-        (<p>Please sign in</p>)}
-      </div>
-    )
-  }
+        this.state = {
+            isLoaded: true,
+            isLoggedIn: true,
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <UserData isLoaded={this.state.isLoaded}/>
+                <UserMessage isLoggedIn={this.state.isLoggedIn} />
+            </div>
+        )
+    }
 
 }
 
