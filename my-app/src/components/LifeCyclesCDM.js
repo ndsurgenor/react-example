799a7e8b67be7a735e4fe8ab97af4043797dce3b
Nathan Surgenor <ndsurgenor@gmail.com>
2023-12-01 22:32:30 +0000

    Add LifeCyclesCDU components

--- my-app/src/components/LifeCyclesCDM.js
+++ my-app/src/components/LifeCyclesCDM.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import LifeCyclesCDMChild from './LifeCyclesCDMChild'
 
 export class LifeCyclesCDM extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            data: 'loading',
+        }
+    }
+
+    getData() {
+        setTimeout(() => {
+            this.setState({
+                data: 'loaded',
+            })
+        }, 3000)
+    }
+
+    componentDidMount() {
+        this.getData()
+    }
+
     render() {
         return (
             <div>
-                <h1>lifecycles</h1>
+                <h1>{this.state.data}</h1>
+                <LifeCyclesCDMChild />
             </div>
         )
     }
