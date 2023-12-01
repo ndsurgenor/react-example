61f1a5dc7f2fb9f55c2d4ca836b2b6a6bc37171f
Nathan Surgenor <ndsurgenor@gmail.com>
2023-12-01 22:45:53 +0000

    Add updateGreeting function

--- my-app/src/components/LifeCyclesCDU.js
+++ my-app/src/components/LifeCyclesCDU.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import LifeCyclesCDUChild from './LifeCyclesCDUChild'
 
 export class LifeCyclesCDU extends Component {
+
     constructor(props) {
 
         super(props)
@@ -9,13 +11,24 @@ export class LifeCyclesCDU extends Component {
         }
     }
 
+    updateGreeting = () => {
+        this.setState(prevState => {
+            return {
+                greeting: prevState.greeting === 'Hello!' ? 'Goodbye' : 'Hello!'
+            }
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.state.greeting}</h1>
+                <button onClick={this.updateGreeting}>Update Greeting</button>
+                <LifeCyclesCDUChild parentGreeting={this.state.greeting}/>
             </div>
         )
     }
+
 }
 
 export default LifeCyclesCDU
