f259c686cd83db0aa0377715792cabe678d76d3c
Nathan Surgenor <ndsurgenor@gmail.com>
2023-12-04 10:54:07 +0000

    Add UseState hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import ControlledFormHooks from "./components/ControlledFormHooks";
+import UseStateWithObjects from "./components/UseStateWithObjects";
 
 function App() {
   return <div className="App">
-      <ControlledFormHooks />
+      <UseStateWithObjects />
   </div>;
 }
 
---
+++ my-app/src/components/UseStateWithArrays.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+function UseStateWithArrays() {
+
+    const [nums, setNums] = useState([1, 2, 3]);
+    const addNums = () => {
+        setNums([...nums, nums.length + 1]);
+    }
+    const removeNums = () => {
+        setNums(nums.filter((item, i) => {
+        return i !== nums.length - 1
+    }));
+}
+
+return (
+    <div>
+        <button onClick={addNums}>Add Item</button>
+        <button onClick={removeNums}>Remove Item</button>
+        <ul>
+            {nums.map(num => <li key={num}>{num}</li>)}
+        </ul>
+    </div>
+)
+
+}
+
+export default UseStateWithArrays
