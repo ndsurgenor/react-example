51515f0e258b090fb255ee83c98817e7acaa1b64
Nathan Surgenor <ndsurgenor@gmail.com>
2023-12-04 14:03:20 +0000

    Add HTTPRequest.js and get data from API

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import UseStateWithObjects from "./components/UseStateWithObjects";
+import HTTPRequests from "./components/HTTPRequests";
 
 function App() {
   return <div className="App">
-      <UseStateWithObjects />
+      <HTTPRequests />
   </div>;
 }
 
